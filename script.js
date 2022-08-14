1bd406601cfcbf54c73058391f03aebd782c2d22
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-08-13 19:57:26 -0700

    Creating mobile functionality to sift through work experience

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const all = document.querySelector('*');
 const body = document.querySelector('body');
+const navDots = document.querySelectorAll('.dots');
+const works = document.querySelectorAll('.work');
+const worksContainer = document.querySelector('.work-experience');
+let start;
+let end;
+let count = 0;
+
+const moveIt = () => {
+ //const width = works[0].clientWidth;
+  if (start > end) count--;
+  if (start < end) count++;
+  works.forEach(work => work.style.transform = 'translateX(' + count + '00%)');
+};
 
 body.addEventListener('dblclick', (e) => {
     e.preventDefault();
     all.classList.toggle('dark-mode');
 });
+
+works.forEach(work => work.addEventListener('touchstart', (e) => start = e.touches[0].pageX));
+works.forEach(work => work.addEventListener('touchmove', (e) => end = e.touches[0].pageX));
+works.forEach(work => work.addEventListener('touchend', moveIt));
