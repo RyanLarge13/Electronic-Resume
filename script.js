e12b84f5fe12d74a72ecb80ce0648f7cc5a91753
Ryan Large <ryanhudsonlarge13@gmail.com>
2022-08-13 22:25:06 -0700

    Updating work experience functonality.

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 const all = document.querySelector('*');
 const body = document.querySelector('body');
-const navDots = document.querySelectorAll('.dots');
+const navDots = document.querySelectorAll('.dot');
 const works = document.querySelectorAll('.work');
 const worksContainer = document.querySelector('.work-experience');
 let start;
 let end;
 let count = 0;
 
+const color = () => {
+  if (all.classList.contains("dark-mode"))
+    navDots.forEach((dot) => (dot.style.backgroundColor = "#fff"));
+  else
+    navDots.forEach((dot) => (dot.style.backgroundColor = "rgba(0, 0, 0, .6)"));
+};
+
+const navIndicate = (count) => {
+  color();
+  navDots[count].style.backgroundColor = 'rgba(100, 0, 0, .7';
+};
+
 const moveIt = () => {
-  if (start > end) count--;
-  if (start < end) count++;
-  works.forEach(work => work.style.transform = `translateX(${count}00%)`);
+  if (start > end && count < 4) count++;
+  if (start < end && count > 0) count--;
+  works.forEach(work => work.style.transform = `translateX(-${count}00%)`);
+  navIndicate(count);
 };
 
 body.addEventListener('dblclick', (e) => {
     e.preventDefault();
     all.classList.toggle('dark-mode');
+    navIndicate(count);
 });
-
+navDots[0].style.backgroundColor = 'rgba(100, 0, 0, .7';
 works.forEach(work => work.addEventListener('touchstart', (e) => start = e.touches[0].pageX));
 works.forEach(work => work.addEventListener('touchmove', (e) => end = e.touches[0].pageX));
 works.forEach(work => work.addEventListener('touchend', moveIt));
