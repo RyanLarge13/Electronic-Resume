9b73e4b35cec1ed52a7f120135703ee43674f8c7
RyanLarge13 <ryanlarge13@gmail.com>
2023-02-17 03:53:14 -0800

    Fixing The Translate Function!
    When a user moved the work history right or left with a touch action, the start and end variables were not properly being reset neither were they being checked
    if the variable was equal to null/undefined nor if it was reset and no touch event was fired.
    
    This cause an unexpected side effect where if a user moved the slider left or right and then tapped on it the move event would not fire leaving it as the
    same value as before and causing the slider to move when not prompted to.
    
    A few styles were also updated for better responsiveness.

--- script.js
+++ script.js
@@ -20,18 +20,32 @@ const navIndicate = (count) => {
 };
 
 const transWork = (index) => {
-	start = 0;
-	end = 0
+  start = 0;
+  end = 0;
   count = index;
   moveIt();
 };
 
 const moveIt = () => {
-  if (start > end && start - end > 75 && count < 4) count++;
-  if (start < end && end - start > 75 && count > 0) count--;
+  const gap = 75;
+  if (start === undefined || end === undefined || start === 0 || end === 0)
+    return;
+  if (start - end > gap && count < 4) {
+    count++;
+    return moveWorkHistory();
+  }
+  if (end - start > gap && count > 0) {
+    count--;
+    return moveWorkHistory();
+  }
+};
+
+const moveWorkHistory = () => {
   works.forEach((work) => (work.style.transition = "500ms ease-in-out"));
   works.forEach((work) => (work.style.transform = `translateX(-${count}00%)`));
   navIndicate(count);
+  end = 0;
+  start = 0;
 };
 
 body.addEventListener("dblclick", (e) => {
